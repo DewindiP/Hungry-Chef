fcb24fb9a6bfb5c6b87a71e7e2e5b371ba365515
Thimiraakalnaka <thimiraakalanka13@icloud.com>
2024-12-25 13:26:02 +0530

    connect real backend and database

--- fetchproduct.js
+++ fetchproduct.js
@@ -3,29 +3,29 @@ const htmlBox = document.getElementById("product-container");
 
 
 
-fetch("https://fakestoreapi.com/products")
+fetch("http://localhost:3001/api/products")
 .then(function(res){
   return res.json();
 }).then(function(data){
-  // console.log(data);
+  console.log(data.response);
 
-  const catalog = data.map((item)=>{
+  const catalog = data.response.map((item)=>{
 
-    let title = item.title;
-    let shorttitle = title.slice(0,15);
+    // let title = item.title;
+    // let shorttitle = title.slice(0,15);
 
-    let description = item.description;
-    let shortDescription = description.slice(0,15);
+    // let description = item.description;
+    // let shortDescription = description.slice(0,15);
 
     
 
     return ` <div class="box1" id="box1">
                 <div class="items">
-                    <img src="${item.image}" alt="${shorttitle}" width="180px" height="150px">
+                    <img src="${item.imgurl}" alt="${item.description}" width="180px" height="150px">
                 </div>
-                <div class="heading">${shorttitle}</div>
-                <p>${shortDescription}</p>
-                <p class = "category">${item.category}<span>Pizza</span></p>
+                <div class="heading">${item.productname}</div>
+                <p>${item.description}</p>
+                <p class = "category">category : <span>${item.category}</span></p>
                 <div class="price">${item.price}</div>
                 <a href="Cart.html">
                  <button class="red_btn" >Add Cart</button>  
@@ -36,3 +36,4 @@ fetch("https://fakestoreapi.com/products")
   htmlBox.innerHTML = catalog;
 
 })
+
